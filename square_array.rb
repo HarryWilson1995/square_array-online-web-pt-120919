d49e4dd63c6ce87dd8db1327f5f616cf50b88aee
Harry Wilson <harry.wilson1995a@gmail.com>
2019-12-11 14:27:51 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   array2 = []
   array.each do |number|
-    num_squared = #{number} ** 2 
+    num_squared = number ** 2 
     array2 << num_squared
     return array2 
   end 
